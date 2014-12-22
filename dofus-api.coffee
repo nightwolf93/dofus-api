24f6a0fe8d6b164f86ccff0e7c7386e105531966
Nicolas Martins <nico.style931@gmail.com>
2014-12-22 17:33:01 +0100

    - login - get account informations like the email

--- dofus-api.coffee
+++ dofus-api.coffee
@@ -1,12 +1,18 @@
 querystring = require('querystring')
 https = require('https')
 fs = require('fs')
-request = require('request')
+FileCookieStore = require('tough-cookie-filestore')
+request = require('request');
+#require('request-debug')(request)
+request = request.defaults({ jar : true })
+
 
 class DofusAPI
   constructor: ->
     @rootUrl = 'http://www.dofus.com/'
     @logged = false
+    @cookies = '';
+    @account = { pseudo: '', email: '' }
 
   login: (username, password, callback) ->
     url = "https://account.ankama.com/sso"
@@ -21,15 +27,50 @@ class DofusAPI
       }
     }, (error, response, body) =>
       if response.headers.location != loginFailedUrl
+        @logged = true
         callback { success: true }
       else
+        @logged = false
         callback { success: false }
 
-window.DofusAPI = DofusAPI
+  choosePseudo: (pseudo) ->
+    if @logged
+      request 'http://www.dofus.com/fr/choisir-votre-pseudo'
+        , {
+        method: 'POST'
+        headers: { "X-Requested-With" : "XMLHttpRequest" }
+        form: {
+          'usernickname' : pseudo
+        }
+      }, (error, response, body) =>
+    else
+      console.log 'DofusAPI::ChoosePseudo -> You must be logged on a account before'
+
+  getAccountInformations: ->
+    if @logged
+      url = "https://account.ankama.com/fr/votre-compte/profil"
+      request.get url, {}, (error, response, body) =>
+        pattern = /<b>(.*?)<\/b>/ig
+        matches = body.match pattern
+
+        @account.pseudo = matches[0].replace(/(<[^>]*>)/g,' ').trim();
+        @account.email = matches[1].replace(/(<[^>]*>)/g,' ').trim();
+        console.log @account
+    else
+      console.log 'DofusAPI::GetAccountInformations -> You must be logged on a account before'
+
+###
+
+  EXPERIMENTAL TEST
+  TOUCH HERE JUST FOR TESTING
+
+###
 
 api = new DofusAPI()
 api.login "justarandomaccount", "justarandompassword", (result) ->
   if result.success
     console.log 'logged'
+    api.getAccountInformations (account) ->
+
   else
     console.log 'failed to login'
--- dofus-api.js
+++ dofus-api.js
@@ -1,5 +1,5 @@
 (function() {
-  var DofusAPI, api, fs, https, querystring, request;
+  var DofusAPI, FileCookieStore, api, fs, https, querystring, request;
 
   querystring = require('querystring');
 
@@ -7,12 +7,23 @@
 
   fs = require('fs');
 
+  FileCookieStore = require('tough-cookie-filestore');
+
   request = require('request');
+
+  request = request.defaults({
+    jar: true
+  });
 
   DofusAPI = (function() {
     function DofusAPI() {
       this.rootUrl = 'http://www.dofus.com/';
       this.logged = false;
+      this.cookies = '';
+      this.account = {
+        pseudo: '',
+        email: ''
+      };
     }
 
     DofusAPI.prototype.login = function(username, password, callback) {
@@ -30,10 +41,12 @@
       }, (function(_this) {
         return function(error, response, body) {
           if (response.headers.location !== loginFailedUrl) {
+            _this.logged = true;
             return callback({
               success: true
             });
           } else {
+            _this.logged = false;
             return callback({
               success: false
             });
@@ -42,17 +55,60 @@
       })(this));
     };
 
+    DofusAPI.prototype.choosePseudo = function(pseudo) {
+      if (this.logged) {
+        return request('http://www.dofus.com/fr/choisir-votre-pseudo', {
+          method: 'POST',
+          headers: {
+            "X-Requested-With": "XMLHttpRequest"
+          },
+          form: {
+            'usernickname': pseudo
+          }
+        }, (function(_this) {
+          return function(error, response, body) {};
+        })(this));
+      } else {
+        return console.log('DofusAPI::ChoosePseudo -> You must be logged on a account before');
+      }
+    };
+
+    DofusAPI.prototype.getAccountInformations = function() {
+      var url;
+      if (this.logged) {
+        url = "https://account.ankama.com/fr/votre-compte/profil";
+        return request.get(url, {}, (function(_this) {
+          return function(error, response, body) {
+            var matches, pattern;
+            pattern = /<b>(.*?)<\/b>/ig;
+            matches = body.match(pattern);
+            _this.account.pseudo = matches[0].replace(/(<[^>]*>)/g, ' ').trim();
+            _this.account.email = matches[1].replace(/(<[^>]*>)/g, ' ').trim();
+            return console.log(_this.account);
+          };
+        })(this));
+      } else {
+        return console.log('DofusAPI::GetAccountInformations -> You must be logged on a account before');
+      }
+    };
+
     return DofusAPI;
 
   })();
 
-  window.DofusAPI = DofusAPI;
+
+  /*
+  
+    EXPERIMENTAL TEST
+    TOUCH HERE JUST FOR TESTING
+   */
 
   api = new DofusAPI();
 
   api.login("justarandomaccount", "justarandompassword", function(result) {
     if (result.success) {
-      return console.log('logged');
+      console.log('logged');
+      return api.getAccountInformations(function(account) {});
     } else {
       return console.log('failed to login');
     }
